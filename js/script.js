7890d7b32c4a50820e63ccaf7480b5df7314b5b0
Vitalii <duvit@users.noreply.github.com>
2023-02-01 02:47:05 +0200

    upload

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ for (i = 0; i < filterArr.length; i++) {
   });
 }
 
+//Slider script
 var slideIndex = 0;
 showSlides();
 
@@ -32,36 +33,13 @@ function showSlides() {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
-  // dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-}
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  // dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 3000);
 }
 
 const itemsArr = [...items2];
+const htmlContainer = document.querySelector(".container");
 
 const cardTemplate = document.querySelector("#cardTemplate");
-const htmlContainer = document.querySelector(".container");
 
 function renderItemCard(item) {
   let div = document.createElement("div");
